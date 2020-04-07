b534546663108f906e419349039cf481e2a69582
trevi51 <61690745+trevi51@users.noreply.github.com>
2020-04-07 07:36:43 +0300

    final work

---
+++ web.js
@@ -0,0 +1,15 @@
+localStorage.setItem("registration.html")
+console.log(localStorage.setItem("registration.htmml", "student_login.html", "index.html"))
+
+localStorage.setItem( "student_login.html")
+console.log(localStorage.setItem("student_login.html"))
+
+localStorage.setItem( "index.html")
+console.log(localStorage.setItem ("index.html"))
+
+sessionStorage.setItem("registration.html")
+
+sessionStorage.setItem("student_login.html")
+
+sessionStorage.setItem("index.html")
+
